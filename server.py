f609c69508cbb89b60fa73837fa8c836f2dd3836
rishutoshniwal <rishutoshniwal73@gmail.com>
2022-01-03 11:55:53 +0530

    working fine

--- server.py
+++ server.py
@@ -6,22 +6,63 @@ from flask_session import Session
 import random
 from passlib.hash import sha256_crypt
 from datetime import timedelta
-# from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
+from sqlalchemy import desc
 
-engine=create_engine("mysql+pymysql://root:Rishu0703@localhost/ssd3")
-db=scoped_session(sessionmaker(bind=engine))
+
+
 
 app = Flask(__name__)
 app.secret_key='rishutoshniwal'
-# login_manager=LoginManager()
-# login_manager.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
 
-loggedin=False
-loggedinUserName=''
-i=0
-start=db.execute("SELECT count(*) from ordermap").fetchone()
-for c in start:
-    i=c
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+@login_manager.user_loader
+def load_user(username):
+    return Users.query.get(username)
+
+
+class Users(UserMixin,db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False,primary_key=True)
+    password = db.Column(db.String(120), nullable=False)
+    usertype=  db.Column(db.String(120), nullable=False)
+
+    def get_id(self):
+           return (self.username)
+
+class Menu(db.Model):
+    itemno= db.Column(db.Integer, primary_key=True,unique=True, nullable=False)
+    halfplate=db.Column(db.Integer, nullable=False)
+    fullplate=db.Column(db.Integer, nullable=False)
+
+class Ordermap(db.Model):   
+    orderid=db.Column(db.Integer, primary_key=True,unique=True, nullable=False)
+    username = db.Column(db.String(80),nullable=False)
+    tip=db.Column(db.Integer, nullable=False)
+    people=db.Column(db.Integer, nullable=False)
+    discount=db.Column(db.Integer, nullable=False)
+
+class Transaction(db.Model):
+    orderid=db.Column(db.Integer, db.ForeignKey('ordermap.orderid'), nullable=False,primary_key=True) 
+    itemno=db.Column(db.Integer,db.ForeignKey('menu.itemno'), nullable=False,primary_key=True)  
+    platetype=db.Column(db.String(80), nullable=False,primary_key=True)
+    quantity=db.Column(db.Integer, nullable=False)
+    total=db.Column(db.Integer, nullable=False)
+
+
+
+
+
+# loggedin=False
+# loggedinUserName=''
+
 
 
 @app.route("/", methods =['GET', 'POST'])
@@ -32,28 +73,24 @@ def index():
              
 @app.route('/register', methods = [ 'GET','POST'])
 def register():
-    global loggedin
-    if loggedin==True:
-        message=[]
-        message.append("A new user can login only when current user logout")
-        return redirect(url_for('index',message=message))
     if request.method=="POST":
         data = request.form
         username = data['username']
         pass1 = data['password']
         type1 = data['type']
-        check=db.execute("SELECT username from users WHERE username=:username",{"username":username}).fetchone()
-            
-        
+        check=Users.query.filter_by(username=username).first()
+
         if check!=None:
             message=[]
             message.append("You are already registered, you can login with your credentials")
             return redirect(url_for('login',message=message))
 
-        db.execute("INSERT INTO users(username,password,usertype) VALUES(:username,:pass1,:type1)",
-                {"username":username,"pass1":pass1,"type1":type1})
-        db.commit()
-    
+        
+        obj=Users(username=username,password=pass1,usertype=type1)
+        db.session.add(obj)
+        db.session.commit()
+
+        
         message=[]
         message.append("New user registered succesfully")
         return redirect(url_for('login',message=message))
@@ -61,19 +98,15 @@ def register():
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
-    global loggedin
-    if loggedin==True:
-        message=[]
-        message.append("You are already logged in")
-        return redirect(url_for('index',message=message))
+    
+
     if request.method=="POST":
         data = request.form
         username = data['username']
         pass1 = data['password']
     
 
-        check=db.execute("SELECT * from users WHERE username=:username",{"username":username}).fetchone()
-        
+        check=Users.query.filter_by(username=username).first()
        
 
         if check is None :
@@ -82,23 +115,17 @@ def login():
             return redirect(url_for('register',message=message))
     
         else:
-            currentLoginUserType=check['usertype']
-            if pass1==check['password']:
-            
-                session['loggedin']=True
-                session['id']=check['username']
-                session['username']=check['username']
-                global loggedinUserName
-                
-                loggedin=True
-                loggedinUserName=check['username']
+            currentLoginUserType=check.usertype
+            if pass1==check.password:
                 if(currentLoginUserType=="chef"):
                     message=[]
                     message.append("Chef , Successfully Logged in")
+                    login_user(check)
                     return redirect(url_for('index',message=message))
                 else :  
                    message=[]   
                    message.append("Customer , Successfully Logged in")
+                   login_user(check)
                    return redirect(url_for('index',message=message))
             else:
                 message=[]
@@ -108,56 +135,54 @@ def login():
 
 @app.route('/logout', methods =['GET', 'POST'])	
 def logout():
-    global loggedin
-    global loggedinUserName
-    if loggedin==False:
-         message=[]
-         message.append("User already logged out, login again to perform any action")
-         return redirect(url_for('login',message=message))
-    else:  
-        loggedin=False
-        loggedinUserName=''
+    
+    if current_user.is_authenticated:
         message=[]
         message.append("Logout successfull")
+        logout_user()
         return redirect(url_for('login',message=message))	
+    else:
+        message=[]
+        message.append("User already logged out, login again to perform any action")
+        return redirect(url_for('login',message=message))
+
 
 @app.route('/show_menu', methods =['GET', 'POST'])	
 def show_menu():
-    global loggedin
-    if loggedin==False:
+    
+    if current_user.is_authenticated==False:
         message=[]
         message.append("You need to login first")
         return redirect(url_for('login',message=message))
     
-    # res="itemno"+"\t\t"+"halfplate"+"\t\t"+"fullplate\n"
-    menu=db.execute("SELECT * from menu").fetchall()
+   
+    menu=Menu.query.all()
     return render_template("show_menu.html",menu=menu)
     
 
 
 @app.route('/previous_order', methods =['GET', 'POST'])	
 def previous_order():
-    global loggedin
-    if loggedin==False:
+    if current_user.is_authenticated==False:
         message=[]
         message.append("You need to login first")
         return redirect(url_for('login',message=message))
-    global loggedinUserName
-    username=loggedinUserName
-    orderlist=db.execute("SELECT orderid from ordermap WHERE username=:username",{"username":username}).fetchall()
+    username=current_user.get_id()
+    orderlist=Ordermap.query.filter_by(username=username).all()
     return render_template("previous_orders.html",orderlist=orderlist,username=username)
 
 @app.route('/insert_menu', methods =['GET', 'POST'])	
 def insert_menu():  
-    global loggedin
-    if loggedin==False:
+    if current_user.is_authenticated==False:
         message=[]
         message.append("You need to login first")
         return redirect(url_for('login',message=message))
-    global loggedinUserName
-    username=loggedinUserName
-    check=db.execute("SELECT usertype from users WHERE username=:username",{"username":username}).fetchone()
-    if check['usertype']=="customer":
+
+    
+    username=current_user.get_id()
+    check=Users.query.filter_by(username=username).first()
+
+    if check.usertype=="customer":
         message=[]
         message.append("Only chefs are allowed to perform this operation")
         return redirect(url_for('index',message=message))
@@ -173,17 +198,20 @@ def insert_menu():
             halfplate=int(halfplateList[i])
             fullplate=int(fullplateList[i])
 
-            check=db.execute("SELECT * from menu WHERE itemno=:itemno",{"itemno":itemno}).fetchone()
+            check=Menu.query.filter_by(itemno=itemno).first()
             if check!=None:
-                db.execute("Update menu set halfplate=:halfplate,fullplate=:fullplate WHERE itemno=:itemno"
-            ,{"itemno":itemno,"halfplate":halfplate,"fullplate":fullplate})
+                check.itemno=itemno
+                check.halfplate=halfplate
+                check.fullplate=fullplate
+                db.session.add(check)
+                db.session.commit()
             
             else:
-                db.execute("INSERT INTO menu(itemno,halfplate,fullplate) VALUES(:itemno,:halfplate,:fullplate)",
-                        {"itemno":itemno,"halfplate":halfplate,"fullplate":fullplate})
-            db.commit()
+                obj=Menu(itemno=itemno,halfplate=halfplate,fullplate=fullplate)
+                db.session.add(obj)
+                db.session.commit()
         message=[]
-        message.append("Item added to menu succesfully")
+        message.append("Items added to menu succesfully")
         return render_template("add_items_to_menu.html",message=message)
 
     return render_template("add_items_to_menu.html")
@@ -192,94 +220,89 @@ def insert_menu():
 @app.route('/fetch_that_bill/<orderid>/<username>', methods =['GET', 'POST'])	
 def fetch_that_bill(orderid,username): 
     
-     check=db.execute("SELECT tip from ordermap WHERE (orderid=:orderid and username=:username)",{"orderid":orderid,"username":username}).fetchone()
-     tip=check['tip']
-     check=db.execute("SELECT people from ordermap WHERE (orderid=:orderid and username=:username)",{"orderid":orderid,"username":username}).fetchone()
-     people=check['people']  
-     check=db.execute("SELECT discount from ordermap WHERE (orderid=:orderid and username=:username)",{"orderid":orderid,"username":username}).fetchone()
-     discount=check['discount'] 
-     check=db.execute("SELECT sum(total) from transaction WHERE orderid=:orderid",{"orderid":orderid}).fetchone()
-     subtotal=check['sum(total)']  
-     orderlist=db.execute("SELECT itemno,platetype,quantity,total from transaction WHERE orderid=:orderid",{"orderid":orderid}).fetchall()
+    check=Ordermap.query.filter_by(orderid=orderid ,username=username).first()
+    tip=check.tip
+    people=check.people  
+    discount=check.discount
     
-     val=float(subtotal)* ((100 + float(tip) )/100) * ((100 + float(discount) )/100)
-     total=str('%.2f'%val)
-     val=val/people
-     sharePerHead=str('%.2f'%val)
+    orderlist=Transaction.query.filter_by(orderid=orderid).all()
+    subtotal=0
+    for order in orderlist:
+        subtotal=subtotal+order.total  
+    
+    val=float(subtotal)* ((100 + float(tip) )/100) * ((100 + float(discount) )/100)
+    total=str('%.2f'%val)
+    val=val/people
+    sharePerHead=str('%.2f'%val)
     
 
-     return render_template('bill.html',orderid=orderid,username=username,tip=tip,people=people,subtotal=subtotal,discount=discount,orderlist=orderlist,total=total,sharePerHead=sharePerHead)
+    return render_template('bill.html',orderid=orderid,username=username,tip=tip,people=people,subtotal=subtotal,discount=discount,orderlist=orderlist,total=total,sharePerHead=sharePerHead)
        
     
-
-@app.route('/ordermap', methods =['GET', 'POST'])	
-def ordermap():
-    global i
-    i=i+1
-    
-    global loggedinUserName
-    username = loggedinUserName
-    db.execute("INSERT INTO ordermap(username,orderid) VALUES(:username,:orderid)",{"username":username,"orderid":i})
-    db.commit()
-    return str(i)
     
 
 @app.route('/insert_order', methods =['GET', 'POST'])	
 def insert_order():
-    global loggedin
-    if loggedin==False:
+    if current_user.is_authenticated==False:
         message=[]
         message.append("You need to login first")
         return redirect(url_for('login',message=message))
         
     if request.method=="POST":
-        
         data = request.form
-        global loggedinUserName
-        orderid=int(ordermap())
+        username=current_user.get_id()
+        tip=data['tip']
+        people=data['people']
+        discount=0
+        if data['play_luck_game']=="yes":
+            arr_of_luck=[-50,-25,-25,-10,-10,-10,0,0,0,0,20,20,20,20,20,20,20,20,20,20]
+            index=random.randint(0,19)
+            discount=arr_of_luck[index]
+        
+        maxOrderId=0
+        allRows=Ordermap.query.all()
+        for row in allRows:
+            if row.orderid > maxOrderId:
+                maxOrderId=row.orderid
+
+       
+        orderid=maxOrderId+1
+
+        obj=Ordermap(orderid=orderid,username=username,tip=tip,people=people,discount=discount)
+        db.session.add(obj)
+        db.session.commit()
 
         itemnoList=data.getlist('itemno[]')
         platetypeList=data.getlist('platetype[]')
         quantityList=data.getlist('quantity[]')
+
         length=len(itemnoList)
-        print(length)
+
         for i in range(length):
             itemno=int(itemnoList[i])
             platetype=platetypeList[i]
             quantity=int(quantityList[i])
-            costPerUnit=db.execute("select fullplate from menu where itemno=:itemno",{"itemno":itemno}).fetchone()
+            itemrow=Menu.query.filter_by(itemno=itemno).first()
+            costPerUnit=itemrow.fullplate
+            
             if(platetype=="half"):
-                costPerUnit=db.execute("select halfplate from menu where itemno=:itemno",{"itemno":itemno}).fetchone()
-
-            total=0
-            for c in costPerUnit:
-                total=c*quantity
-            check=db.execute("select quantity,total from transaction where (orderid=:orderid and itemno=:itemno and platetype=:platetype)",{"orderid":orderid,"itemno":itemno,"platetype":platetype}).fetchone()
+                costPerUnit=itemrow.halfplate
+            
+            
+               
+           
+            total=costPerUnit*quantity
+            check=Transaction.query.filter_by(orderid=orderid,itemno=itemno,platetype=platetype).first()
             
             if check is None :
-                db.execute("INSERT INTO transaction(orderid,itemno,platetype,quantity,total) VALUES(:orderid,:itemno,:platetype,:quantity,:total)",{"orderid":orderid,"itemno":itemno,"platetype":platetype,"quantity":quantity,"total":total})
-                db.commit()	 
+                obj=Transaction(orderid=orderid,itemno=itemno,platetype=platetype,quantity=quantity,total=total)
+                db.session.add(obj)
+                db.session.commit()
             else :
-                db.execute("delete from transaction where (orderid=:orderid and itemno=:itemno and platetype=:platetype)",{"orderid":orderid,"itemno":itemno,"platetype":platetype})
-                db.commit()
-                # for c in check:
-                quantity=quantity+check['quantity']
-                total=total+check['total']
-                    
-                db.execute("INSERT INTO transaction(orderid,itemno,platetype,quantity,total) VALUES(:orderid,:itemno,:platetype,:quantity,:total)",{"orderid":orderid,"itemno":itemno,"platetype":platetype,"quantity":quantity,"total":total})
-                db.commit()	 
-            username=loggedinUserName    
-            tip=data['tip']
-            people=data['people']
-            discount=0
-            if data['play_luck_game']=="yes":
-                arr_of_luck=[-50,-25,-25,-10,-10,-10,0,0,0,0,20,20,20,20,20,20,20,20,20,20]
-                index=random.randint(0,19)
-                discount=arr_of_luck[index]
-
-            db.execute("Update ordermap set tip=:tip,people=:people,discount=:discount WHERE (username=:username and orderid=:orderid)"
-            ,{"username":username,"orderid":orderid,"tip":tip,"people":people,"discount":discount})
-            db.commit()	       
+                check.quantity=check.quantity+quantity
+                check.total=check.total+total
+                db.session.add(check)
+                db.session.commit()
 
         return redirect(url_for("fetch_that_bill",orderid=orderid,username=username))
     return render_template("placeOrder.html")
